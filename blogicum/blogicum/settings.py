99c595f8b3f9d5c09008876a09ca07422252dee3
Павел Муратов <grtpbs2@yandex.ru>
2023-09-19 22:12:24 +0300

    19September2

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig'
-    'pages.apps.PagesConfig'
+    'blog.apps.BlogConfig',
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
